1d942ccb8993ee080263a1c5eb79a0d7f3fea154
longade <davide.longo97@gmail.com>
2020-11-01 21:58:34 +0100

    added Selenium and captured network traffic in har file

--- src/main/java/com/longade/connection/HttpConnection.java
+++ src/main/java/com/longade/connection/HttpConnection.java
@@ -1,11 +1,24 @@
 package com.longade.connection;
 
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +27,7 @@ public class HttpConnection {
 
     URL url;
     HttpsURLConnection connection;
+    BrowserMobProxy browserMobProxy;
 
     public void getRequest(String strURL) {
         try {
@@ -25,25 +39,77 @@ public class HttpConnection {
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        finally {
+            connection.disconnect();
+        }
     }
 
     public List<String> getHTML(String strURL) {
 
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+
+        browserMobProxy = new BrowserMobProxyServer();
+        browserMobProxy.setTrustAllServers(true);
+        browserMobProxy.start(9090);
+        System.out.println(browserMobProxy.getPort());
+
+        Proxy proxy = ClientUtil.createSeleniumProxy(browserMobProxy);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setProxy(proxy);
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        /* java.net Proxy to use for Jsoup connect (SSL problems) */
+        // SocketAddress socketAddress = new InetSocketAddress("localhost", browserMobProxy.getPort());
+        // Proxy proxy = new Proxy(Proxy.Type.HTTP, socketAddress);
+
         List<String> urls = new ArrayList<>();
 
         try {
-            Document doc = Jsoup.connect(strURL).get();
+            browserMobProxy.newHar("google.it");
+            driver.get("https://www.google.it");
+            Thread.sleep(5000);
+            Har har = browserMobProxy.getHar();
+            File harFile = new File("network_requests.har");
+            har.writeTo(harFile);
+            return null;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        finally {
+            driver.close();
+            browserMobProxy.stop();
+        }
+
+        // use of Jsoup to connect to the page and get html elements
+        /* try {
+            browserMobProxy.newHar("davewebsite");
+            Document doc = Jsoup.connect("http://onlyfoot.net/ch3sport.html")
+                    .proxy(proxy)
+                    .get();
             Elements list = doc.select("ul > li");
             for (Element single : list) {
                 System.out.println(single.text());
                 urls.add(single.text());
             }
+            Har har = browserMobProxy.getHar();
+            File harFile = new File("network_requests.har");
+            har.writeTo(harFile);
             return urls;
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
+        finally {
+            browserMobProxy.stop();
+        }*/
+
     }
 
 }
